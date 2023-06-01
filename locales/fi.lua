bf1a037da5491777f6a331473c16511d62f08c09
Ap1na <D4cky0@gmail.com>
2023-06-01 17:28:32 +0300

    Update fi.lua
    forgot to add 1 thing

--- locales/fi.lua
+++ locales/fi.lua
@@ -20,7 +20,10 @@ local Translations = {
     },
 }
 
-Lang = Lang or Locale:new({
-    phrases = Translations,
-    warnOnMissing = true
-})
+if GetConvar('qb_locale', 'en') == 'fi' then
+    Lang = Locale:new({
+        phrases = Translations,
+        warnOnMissing = true,
+        fallbackLang = Lang,
+    })
+end
